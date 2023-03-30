4eee8029747d7e8c5bd46f9200112bf1c0452464
Shaun Nelson <shaunptnelson@gmail.com>
2023-03-30 04:07:43 -0400

    Added time blocks to HTML, added localstorage, added colours to time-blocks based on hour, added persistent user input, added current date in header

--- Assets/script.js
+++ Assets/script.js
@@ -8,16 +8,45 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".saveBtn").click(function () {
+    var hour = $(this).closest("div").attr("id").slice(5);
+    var text = $(this).siblings("textarea").val();
+    localStorage.setItem(hour, text);
+  });
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var currentHour = dayjs().hour();
+  var timeBlocks = $(".time-block");
+
+  for (var i = 0; i < timeBlocks.length; i++) {
+    //Gets the id attribute of each time-block and removes "hour-", giving
+    //the hour as a number.
+    var id = $(timeBlocks[i]).attr("id").slice(5);
+
+    if (currentHour > id) {
+      $(timeBlocks[i]).attr("class", "row time-block past");
+    } else if (currentHour === id) {
+      $(timeBlocks[i]).attr("class", "row time-block present");
+    } else {
+      $(timeBlocks[i]).attr("class", "row time-block future");
+    }
+  }
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var hour = $(timeBlocks[i]).attr("id").slice(5);
+
+    if (localStorage.getItem(hour)) {
+      $(timeBlocks[i]).children("textarea").html(localStorage.getItem(hour));
+    }
+  }
+
   // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("ddd, MMM D, YYYY "));
 });
